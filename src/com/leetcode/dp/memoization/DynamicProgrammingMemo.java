b2f3895cab3789394c70e3e057650b871b0a2674
ivanovaolya <olya.ivanova.kiev@gmail.com>
2021-08-12 14:59:13 +0300

    Dynamic programming - grid traveler (memoization/tabulation)

--- src/com/leetcode/dp/memoization/DynamicProgrammingMemo.java
+++ src/com/leetcode/dp/memoization/DynamicProgrammingMemo.java
@@ -1,5 +1,7 @@
 package com.leetcode.dp.memoization;
 
+import com.leetcode.dp.memoization.data.Coordinate;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,5 +24,40 @@ public class DynamicProgrammingMemo {
         memo.put(n, res);
 
         return res;
+    }
+
+    /**
+     * Given a 2-D matrix with M rows and N columns,
+     * find the number of ways to reach cell with coordinates (i,j) from starting cell (0,0)
+     * under the condition that you can only travel one step right or one step down.
+     *
+     * @param x destination X coordinate
+     * @param y destination Y coordinate
+     *
+     * @return number of ways
+     */
+    public long gridTraveler(int x, int y) {
+        Map<Coordinate, Long> memo = new HashMap<>();
+
+        return gridTraveler(x, y, memo);
+    }
+
+    private long gridTraveler(int x, int y, Map<Coordinate, Long> cache) {
+        if (x == 1 && y == 1) {
+            return 1;
+        } else if (x == 0 || y == 0) {
+            return 0;
+        }
+
+        Coordinate coordinate = new Coordinate(x, y);
+
+        if (cache.containsKey(coordinate)) {
+            return cache.get(coordinate);
+        }
+
+        long steps = gridTraveler(x - 1, y, cache) + gridTraveler(x, y - 1, cache);
+        cache.put(coordinate, steps);
+
+        return steps;
     }
 }
--- src/com/leetcode/dp/tabulation/DynamicProgrammingTab.java
+++ src/com/leetcode/dp/tabulation/DynamicProgrammingTab.java
@@ -12,4 +12,24 @@ public class DynamicProgrammingTab {
         }
         return table[table.length - 1];
     }
+
+    public long gridTraveler(int x, int y) {
+        long [][] matrix = new long[x + 1][y + 1];
+        matrix[1][1] = 1;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                long current = matrix[i][j];
+
+                if (j + 1 < matrix[i].length) {
+                    matrix[i][j + 1] += current;
+                }
+                if (i + 1 < matrix.length) {
+                    matrix[i + 1][j] += current;
+                }
+            }
+        }
+
+        return matrix[x][y];
+     }
 }
